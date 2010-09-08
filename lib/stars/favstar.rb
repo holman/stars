fdcbbcb80fe7565d60dc92a6ff7c6bce6dda51dd
wmernagh <will.mernagh@lmco.com>
2010-09-08 14:47:32 -0400

    Added http_proxy based on the ENV
    Fixed the gets issue by explicitly using STDIN.gets.
    gets will read ARGV as a file. When a user is specified in the
    command line call gets tries to load the username as a file and
    expects to read in the first line of the file.
    
    Use * instead of star(0xe29885) since it makes my putty terminal
    all wonky. Will try to revert to star(0xe29885) at some point.

--- lib/stars/favstar.rb
+++ lib/stars/favstar.rb
@@ -3,6 +3,8 @@ module Stars
   class Favstar
     include HTTParty
     base_uri 'favstar.fm'
+    proxy_url = URI.parse(ENV['http_proxy']) if ENV['http_proxy']
+    http_proxy proxy_url.host,proxy_url.port if proxy_url
 
     def recent(username)
       self.class.get("/users/#{username}/rss",
@@ -13,7 +15,7 @@ module Stars
       # hardcode 17 to strip favstar domain for now
       html = self.class.get(url[17..200], :format => :html)
       Nokogiri::HTML(html).css('.avatarList img').collect do |img|
-        "    ★  #{img.attributes['alt'].value}"
+        "    *  #{img.attributes['alt'].value}"
       end
     end
   end
